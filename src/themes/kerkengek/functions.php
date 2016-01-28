285d4ef91692d6b0934165aa567c8d3399c4ec2b
maartenkools <mkools@outlook.com>
2016-01-28 23:09:48 +0100

    Ensured the author in feeds is always 'kerkengek'

--- src/themes/kerkengek/functions.php
+++ src/themes/kerkengek/functions.php
@@ -1,21 +1,29 @@
 <?php
 
-define('KG_THEME_VERSION', '{wp-major}.{wp-minor}.{build}.{revision}');
+define( 'KG_THEME_VERSION', '{wp-major}.{wp-minor}.{build}.{revision}' );
 
-function kerkengek_enqueue_scripts()
-{
-    wp_dequeue_style('bushwick-style');
-    wp_deregister_style('bushwick-style');
+add_filter( 'the_author', 'kerkengek_customize_feed' );
+function kerkengek_customize_feed( $content ) {
+	if ( ! is_feed() ) {
+		return $content;
+	}
 
-    wp_enqueue_style('bushwick-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('kerkengek-style', get_stylesheet_uri(), array('bushwick-style'), KG_THEME_VERSION);
-
-    wp_enqueue_script('kerkengek-functions', get_stylesheet_directory_uri() . '/js/functions.js', array('jquery'), KG_THEME_VERSION);
+	return 'Kerkengek';
 }
 
-add_action('wp_enqueue_scripts', 'kerkengek_enqueue_scripts', PHP_INT_MAX);
+function kerkengek_enqueue_scripts() {
+	wp_dequeue_style( 'bushwick-style' );
+	wp_deregister_style( 'bushwick-style' );
 
-require_once(dirname(__FILE__) . '/updater/theme-update-checker.php');
+	wp_enqueue_style( 'bushwick-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'kerkengek-style', get_stylesheet_uri(), array( 'bushwick-style' ), KG_THEME_VERSION );
+
+	wp_enqueue_script( 'kerkengek-functions', get_stylesheet_directory_uri() . '/js/functions.js', array( 'jquery' ), KG_THEME_VERSION );
+}
+
+add_action( 'wp_enqueue_scripts', 'kerkengek_enqueue_scripts', PHP_INT_MAX );
+
+require_once( dirname( __FILE__ ) . '/updater/theme-update-checker.php' );
 $updateChecker = new ThemeUpdateChecker(
 	'kerkengek',
 	'https://raw.githubusercontent.com/maartenkools/wordpress/master/updater/kerkengek.json'

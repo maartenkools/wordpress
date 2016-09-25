397b915da8820a45873979963fbb2f3505751e99
maartenkools <mkools@outlook.com>
2016-09-25 16:50:19 +0200

    Introduced the 960 grid for articles

--- src/themes/antoniusbavo/template-parts/content.php
+++ src/themes/antoniusbavo/template-parts/content.php
@@ -19,31 +19,22 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php antoniusbavo_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
+			<?php
 			the_content( sprintf(
-				/* translators: %s: Name of current post. */
+			/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'antoniusbavo' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'antoniusbavo' ),
-				'after'  => '</div>',
-			) );
-		?>
+			?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php antoniusbavo_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
+		</div>
+	</div>
 </article><!-- #post-## -->
